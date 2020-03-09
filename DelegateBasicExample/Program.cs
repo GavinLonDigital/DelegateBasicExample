df82e9935fa8bf45488113d558a26c1cf3c8f358
Lon <gavinlondee@gmail.com>
2020-03-09 20:05:21 +0200

    Initial commit

---
+++ DelegateBasicExample/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace DelegateBasicExample
+{
+
+    class Program
+    {
+        delegate void LogDel(string text);
+        static void Main(string[] args)
+        {
+            Log log = new Log();
+
+            LogDel LogTextToScreenDel, LogTextToFileDel;
+
+            LogTextToScreenDel = new LogDel(log.LogTextToScreen);
+
+            LogTextToFileDel = new LogDel(log.LogTextToFile);
+
+            LogDel multiLogDel = LogTextToScreenDel + LogTextToFileDel;
+
+            Console.WriteLine("Please enter your name");
+
+            var name = Console.ReadLine();
+
+            LogText(multiLogDel, name);
+
+            Console.ReadKey();
+        
+        }
+
+        static void LogText(LogDel logDel, string text)
+        {
+            logDel(text);
+        }
+
+    }
+
+    public class Log
+    {
+        public void LogTextToScreen(string text)
+        {
+            Console.WriteLine($"{DateTime.Now}: {text}");
+
+        }
+
+        public void LogTextToFile(string text)
+        {
+            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
+            {
+                sw.WriteLine($"{DateTime.Now}: {text}");
+            }
+        }
+    }
+
+}
